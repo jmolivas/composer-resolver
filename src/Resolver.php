f11238fc2956b1bd0ec2ff694581b5f9c5135933
Yanick Witschi <yanick.witschi@terminal42.ch>
2016-06-15 16:57:29 +0200

    Added predis and some wip stuff

--- src/Resolver.php
+++ src/Resolver.php
@@ -3,17 +3,62 @@
 namespace Toflar\ComposerResolver;
 
 
+use Composer\Factory;
+use Composer\Installer;
+use Composer\IO\NullIO;
+use Predis\Client;
 use Symfony\Component\HttpFoundation\Request;
 
 class Resolver
 {
-    public function post(Request $request)
+    /**
+     * Redis Client
+     * @var Client
+     */
+    private $client;
+
+    /**
+     * Resolver constructor.
+     *
+     * @param Client $client
+     */
+    public function __construct(Client $client)
     {
+        $this->client = $client;
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return string
+     */
+    public function post(Request $request) : string
+    {
+
         return 'you posted';
     }
 
-    public function get($jobId)
+    /**
+     * @param string $jobId
+     *
+     * @return string
+     */
+    public function get(string $jobId) : string
     {
         return 'you wanted job: ' . $jobId;
+    }
+
+    /**
+     *
+     */
+    private function runNext()
+    {
+        $io = new NullIO();
+        $composer = Factory::create($io, null, true);
+        $composer->getInstallationManager()->addInstaller(new Installer\NoopInstaller());
+        $install = Installer::create($io, $composer);
+        $install->setDumpAutoloader(false);
+        $install->run();
+
     }
 }
