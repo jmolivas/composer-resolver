48aad4b73a4592f2d6b66a871c1ba4000832b11f
Yanick Witschi <yanick.witschi@terminal42.ch>
2016-06-30 20:41:11 +0200

    Some bootstrap stuff

--- app/js/components/app.js
+++ app/js/components/app.js
@@ -32,6 +32,47 @@ var AppComponent = React.createClass({
             <div>
                 <p>Docker is available and running on your system: {label}</p>
                 <button onClick={this.checkForDocker}>Check for Docker again.</button>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <h3>Tests</h3>
+                            <div className="panel-group" id="panel-978313">
+                                <div className="panel panel-default">
+                                    <div className="panel-heading">
+                                        <a className="panel-title collapsed" data-toggle="collapse" data-parent="#panel-978313" href="#panel-element-936251">Collapsible Group Item #1</a>
+                                    </div>
+                                    <div id="panel-element-936251" className="panel-collapse collapse">
+                                        <div className="panel-body">
+                                            Anim pariatur cliche...
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="panel panel-default">
+                                    <div className="panel-heading">
+                                        <a className="panel-title" data-toggle="collapse" data-parent="#panel-978313" href="#panel-element-385081">Collapsible Group Item #2</a>
+                                    </div>
+                                    <div id="panel-element-385081" className="panel-collapse collapse in">
+                                        <div className="panel-body">
+                                            Anim pariatur cliche...
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-12">
+                            <h3>composer.json</h3>
+                            <form role="form">
+                                <div className="form-group">
+                                    <label for="composerJson">Enter your composer.json here.</label>
+                                    <textarea className="form-control" id="composerJson" />
+                                </div>
+                                <button type="submit" className="btn btn-default">Submit</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
