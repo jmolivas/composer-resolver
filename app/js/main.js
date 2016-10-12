cc0f49d765120b4244063e5f64ae50d78a2da8e2
Yanick Witschi <yanick.witschi@terminal42.ch>
2016-10-12 16:15:53 +0200

    Removed the electron related stuff

--- app/js/main.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const React         = require('react');
-const ReactDOM      = require('react-dom');
-const App           = require('./components/app.js');
-
-ReactDOM.render(<App />, document.getElementById('app'));
