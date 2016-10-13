a9fa5cedf0cd050f237cf882c590d3d6a1614b95
Yanick Witschi <yanick.witschi@terminal42.ch>
2016-10-13 14:21:25 +0200

    Use the logger service for the worker too

--- bin/worker.php
+++ bin/worker.php
@@ -2,17 +2,17 @@
 
 $app = require_once __DIR__ . '/../app.php';
 
-$writeln = function($ln) { echo $ln . "\n"; };
-
 while(true) {
     /* @var \Predis\Client $predis */
     $predis   = $app['predis'];
+    /** @var \Psr\Log\LoggerInterface $logger */
+    $logger   = $app['logger'];
     $ttl      = $app['redis.jobs.ttl'];
 
     /** @var \Toflar\ComposerResolver\Worker\Resolver $resolver */
     $resolver =  $app['composer-resolver'];
 
-    $writeln('Worker checks for jobs in queue');
+    $logger->info('Worker checks for jobs in queue');
 
     $job = $predis->blpop([$app['redis.jobs.queueKey']], $app['redis.jobs.workerPollingFrequency']);
 
@@ -32,12 +32,16 @@ while(true) {
         try {
             $job = $resolver->resolve($job, $jobIO);
         } catch (\Throwable $t) {
-            $writeln('Error during resolving process: ' . $t->getMessage());
+            $logger->error('Error during resolving process: ' . $t->getMessage(), [
+                'line'  => $t->getLine(),
+                'file'  => $t->getFile(),
+                'trace' => $t->getTrace()
+            ]);
         }
 
         // Finished
         $predis->setex('jobs:' . $job->getId(), $ttl, json_encode($job));
 
-        $writeln('Finished working on job ' . $job->getId());
+        $logger->info('Finished working on job ' . $job->getId());
     }
 }
