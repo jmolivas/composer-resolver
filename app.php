97ed7ee654d0e7c46a36f640c9f9fb82e92f51f1
Yanick Witschi <yanick.witschi@terminal42.ch>
2016-11-15 16:09:35 +0100

    Fixed a few bugs with environment

--- app.php
+++ app.php
@@ -2,20 +2,7 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$app = new Silex\Application();
-
-// Env closure
-$getEnvClosure = function ($key, $default = null) {
-    return function() use ($key, $default) {
-        $envValue = getenv($key);
-
-        if (false !== $envValue) {
-            return $envValue;
-        }
-
-        return $default;
-    };
-};
+$app = new \Toflar\ComposerResolver\Application();
 
 // Console
 $app->register(new Knp\Provider\ConsoleServiceProvider(), array(
@@ -33,11 +20,11 @@ $app->register(new Predis\Silex\ClientServiceProvider(), [
     ],
 ]);
 
-$app['redis.jobs.queueKey']               = $getEnvClosure('COMPOSER_RESOLVER_JOBS_QUEUE_KEY', 'jobs_queue');
-$app['redis.jobs.workerPollingFrequency'] = $getEnvClosure('COMPOSER_RESOLVER_POLLING_FREQUENCY', 5);
-$app['redis.jobs.ttl']                    = $getEnvClosure('COMPOSER_RESOLVER_JOBS_TTL', 600);
-$app['redis.jobs.atpj']                   = $getEnvClosure('COMPOSER_RESOLVER_JOBS_ATPJ', 30);
-$app['redis.jobs.workers']                = $getEnvClosure('COMPOSER_RESOLVER_WORKERS', 1);
+$app['redis.jobs.queueKey']               = $app->env('COMPOSER_RESOLVER_JOBS_QUEUE_KEY', 'jobs_queue');
+$app['redis.jobs.workerPollingFrequency'] = $app->env('COMPOSER_RESOLVER_POLLING_FREQUENCY', 5, 'int');
+$app['redis.jobs.ttl']                    = $app->env('COMPOSER_RESOLVER_JOBS_TTL', 600, 'int');
+$app['redis.jobs.atpj']                   = $app->env('COMPOSER_RESOLVER_JOBS_ATPJ', 30, 'int');
+$app['redis.jobs.workers']                = $app->env('COMPOSER_RESOLVER_WORKERS', 1, 'int');
 
 // Log everything to stout
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
---
+++ src/Application.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Toflar\ComposerResolver;
+
+/**
+ * Class Application
+ *
+ * @package Toflar\ComposerResolver
+ */
+class Application extends \Silex\Application
+{
+    /**
+     * @param string     $key
+     * @param mixed|null $default
+     * @param string     $type
+     *
+     * @return \Closure
+     */
+    public function env($key, $default = null, $type = 'string')
+    {
+        return function () use ($key, $default, $type) {
+            $envValue = getenv($key);
+
+            if (false !== $envValue || '' === $envValue) {
+                settype($envValue, $type);
+                return $envValue;
+            }
+
+            return $default;
+        };
+    }
+}
