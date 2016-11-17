4ffeb3134aa4aec75e6a20499c18d41a81f06ed2
Yanick Witschi <yanick.witschi@terminal42.ch>
2016-11-17 16:56:43 +0100

    Added unit tests for application

---
+++ tests/ApplicationTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Toflar\ComposerResolver;
+
+/**
+ * Class ApplicationTest
+ *
+ * @package Toflar\ComposerResolver
+ */
+class ApplicationTest extends \PHPUnit_Framework_TestCase
+{
+    public function testInstanceOf()
+    {
+        $app = new Application();
+        $this->assertInstanceOf('Toflar\ComposerResolver\Application', $app);
+    }
+
+    /**
+     * @dataProvider envProvider
+     */
+    public function testEnv($env, $key, $default, $type, $expected)
+    {
+        if (null !== $env) {
+            putenv($env);
+        }
+
+        $app = new Application();
+        $app['param'] = $app->env($key, $default, $type);
+        $this->assertSame($expected, $app['param']);
+    }
+
+    public function envProvider()
+    {
+        return [
+            'Test default' => [null, 'FOOBAR', 'test', 'string', 'test'],
+            'Test regular' => ['FOOBAR=value', 'FOOBAR', 'test', 'string', 'value'],
+            'Test cast' => ['FOOBAR=15', 'FOOBAR', 'test', 'integer', 15],
+        ];
+    }
+}
