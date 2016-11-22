980b4498f5942aa82fb1ae8a21a1049105a5c499
Yanick Witschi <yanick.witschi@terminal42.ch>
2016-11-22 15:16:01 +0100

    Removed stuff we don't need

--- app.php
+++ app.php
@@ -4,13 +4,6 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 $app = new \Toflar\ComposerResolver\Application();
 
-// Console
-$app->register(new Knp\Provider\ConsoleServiceProvider(), array(
-    'console.name'              => 'Composer Resolver Console',
-    'console.version'           => '1.0.0',
-    'console.project_directory' => __DIR__
-));
-
 // Redis
 $app->register(new Predis\Silex\ClientServiceProvider(), [
     'predis.parameters' => 'redis:6379',
